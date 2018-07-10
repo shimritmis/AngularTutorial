8eabbfd0df299c55553106a2ae5d7dc6931d64b3
Nir Geier <nirgeier@gmail.com>
2018-07-11 01:27:43 +0300

    step12 AttributeDirective

---
+++ src/app/directives/highlight/highlight.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+// Set the attribute directive
+// The syntax is [<attribute name>]. 
+// When the html will contain the <attribute name> without the brackets
+// This directive will be invoked.
+@Directive({
+    selector: '[appHighlight]'
+    // Directive does not have a template !!!!
+})
+
+/**
+ * We need to implement the OnInit
+ */
+export class HighlightDirective implements OnInit {
+
+    // Get the element which the directive will be applied to
+    // The type is ``ElementRef` and will be references internally as `elementRef`
+    // 
+    // This is called -- injection --
+    // The injection will pass those variables every time the directive will be created
+    constructor(private elementRef: ElementRef) {
+        // The private elementRef will create a property in this class with the given name
+        // so we will be able to access it inside the class
+    }
+
+    ngOnInit() {
+
+        // The logic of the directive. 
+        // This directive will change the backgroundColor
+
+        // this.elementRef = The local variable
+        // nativeElement = The html element
+        this.elementRef.nativeElement.style.backgroundColor = 'green';
+    }
+}
