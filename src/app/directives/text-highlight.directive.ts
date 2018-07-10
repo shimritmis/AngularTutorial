5f00452141046daa707c7e82efac4563c7d72a61
Nir Geier <nirgeier@gmail.com>
2018-07-11 01:47:58 +0300

    step13 DirectiveRenderer

---
+++ src/app/directives/text-highlight.directive.ts
@@ -0,0 +1,26 @@
+// We need to import the requirements for the Renderer
+// Make sure to import Renderer2
+import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appTextHighlight]'
+})
+export class TextHighlightDirective implements OnInit {
+
+  // In this directive we will be using renderer and the element as before
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    // In stead of using the native element we wil use the renderer
+    // The setStyle method get the required element and the style
+    this.renderer.setStyle(
+      this.elRef.nativeElement, // Which element we want to add style to
+      'font-size', // The css property which we wish to assign
+      '24px' // the value of the css property
+      /** Optional 4th param for flex css */
+    )
+  }
+}
