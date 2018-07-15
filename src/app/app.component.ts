92a5ac5819abfafe22b8245d038b068087e52240
Nir Geier <nirgeier@gmail.com>
2018-07-15 22:23:16 +0300

    step19-Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,9 @@
 import { Component } from '@angular/core';
-// component decorator.
-// This component is executed when the application is started
-// Its being executed when the 'ng serve' injet the required JS and imports
-// into the final index.html file and its executing the Angular code
 
 @Component({
-  // This is the entry point for replaceing the content with the 
-  // templates given by the components (html) files.
-  // Each component has its own css, html, js and more which we can reuse later on
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  // set the `serverElements` with intial value
-  serverElements = [{
-    type: 'server',
-    name: 'Testserver',
-    content: 'Test server'
-  }];
-
-  // Set the data which will be passed to this function from the event
-  // Once we pass the object we will add it to the array
-  onServerAdded(serverData: {
-    serverName: string,
-    serverContent: string
-  }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onServerBlueprintAdded(blueprintData: {
-    serverName: string,
-    serverContent: string
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
-  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
 
 @Component({
   selector: 'app-servers',
@@ -6,47 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  private servers: {id: number, name: string, status: string}[] = [];
 
-  // Flag to mark if we can add new server or not
-  allowNewServer = false;
-
-  // The text of the button
-  allowNewServerButtonText = "Create server";
-
-  // Status to konw if server was created
-  serverCreationStatus = "No server was created";
-
-  // the server name which the user will input
-  serverName = "";
-
-  // Flag to mark if we have created a server or not
-  serverCreated = false;
-
-  constructor() {
-
-    // Simulate value change for allowing adding new server
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 5000);
-
-  }
+  constructor(private serversService: ServersService) { }
 
   ngOnInit() {
+    this.servers = this.serversService.getServers();
   }
-
-  // Method for listening when we create new servers
-  // Naming convention: 'On...' for event
-  onCreateServer() {
-    this.serverCreationStatus = "Server was created [" + this.serverName + "]";
-    this.serverCreated = true;
-  }
-
-  // the event is passed from the html when we click on the button
-  // since we added $event to the button
-  onUpdateServerName(event) {
-    // type script code for getting the value
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
 
 }
